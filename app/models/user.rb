96c14fe104f60ad46497c3bf598f22d66a631f0f
beerlington <pete@lette.us>
2013-10-12 10:27:53 -0400

    button hover/clicking styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ActiveRecord::Base
   has_many :eat_events, dependent: :destroy
   has_many :exercise_events, dependent: :destroy
   has_many :outdoor_events, dependent: :destroy
+
+  def eaten_today?
+    eat_events.exists?(event_date: Date.today)
+  end
+
+  def exercised_today?
+    exercise_events.exists?(event_date: Date.today)
+  end
+
+  def outside_today?
+    outdoor_events.exists?(event_date: Date.today)
+  end
 end
 
